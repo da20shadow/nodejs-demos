e5fac3ec3c4d0625f5892a9be4c9897e0fcc69c4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-24 14:42:45 +0200

    Initial Commit
    Node JS Demos

---
+++ firstDemo/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(request,response) => {
+    response.send('Hello World')
+});
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...')
+});
