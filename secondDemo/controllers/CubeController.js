062ac20246b148fe24ddbf0848e627aa786746fa
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-26 10:06:06 +0200

    added saveProduct

--- secondDemo/controllers/CubeController.js
+++ secondDemo/controllers/CubeController.js
@@ -1,3 +1,11 @@
+const Product = require("../models/Product");
 exports.getCreateProductPage = (req, res) => {
     res.render('createProduct');
-}
+};
+
+exports.saveProduct = (req, res) => {
+    const product = new Product(req.body);
+    Product.save(product)
+    console.log(`Product Created! ${product.name} - $ ${product.price}`);
+    res.redirect('/products');
+};
