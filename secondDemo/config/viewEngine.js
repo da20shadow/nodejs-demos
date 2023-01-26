5be1ddb23b0a527ebd467664148041714b9b3ceb
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-26 07:44:30 +0200

    Created view engine setup

---
+++ secondDemo/config/viewEngine.js
@@ -0,0 +1,13 @@
+const handlebars = require("express-handlebars");
+function setupViewEngine(app){
+    //Setting the extension to be .hbs
+    //Here in this config also can be changed the layout directory and partials
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs'
+    }));
+    app.set('view engine', 'hbs');
+    //Set default views folder path
+    app.set('views', './secondDemo/views');
+}
+
+module.exports = setupViewEngine;
