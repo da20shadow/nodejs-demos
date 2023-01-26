ede36d813af58ab01131ce29658a4ba57198e316
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-26 10:05:26 +0200

    updated routes

--- secondDemo/routes.js
+++ secondDemo/routes.js
@@ -1,10 +1,13 @@
 const productController = require("./controllers/CubeController");
+const staticPagesController = require("./controllers/StaticPageController");
 const router = require('express').Router();
 
-router.get('/', (request, response) => {
-    response.render('home');
-});
+router.get('/', staticPagesController.getHomePage);
 
 router.get('/add-product', productController.getCreateProductPage);
+
+router.post('/save-product', productController.saveProduct);
+
+router.get('**', staticPagesController.get404Page);
 
 module.exports = router;
