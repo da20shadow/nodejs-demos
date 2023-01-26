d5d02d225561a0f2a583e3fe220e5102bb6a35e8
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-26 11:31:45 +0200

    added ID for products

--- secondDemo/models/Product.js
+++ secondDemo/models/Product.js
@@ -5,6 +5,7 @@ const db = require('../data/db.json');
 class Product {
 
     constructor(name, description, price, category) {
+        this.id = db.products.length === 0 ? 1 : db.products[db.products.length -1].id + 1;
         this.name = name;
         this.description = description;
         this.price = price;
