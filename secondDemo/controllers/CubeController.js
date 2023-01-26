b9376d5ac52de3509df9baba0e2e05e9a089ff39
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-26 11:45:34 +0200

    Added Details page

--- secondDemo/controllers/CubeController.js
+++ secondDemo/controllers/CubeController.js
@@ -5,6 +5,14 @@ exports.getAllProductsPage = (req, res) => {
     res.render('products',{products: db.products});
 };
 
+exports.getProductDetailsPage = (req, res) => {
+    const productId = Number(req.params.id);
+    if (!productId) { return res.redirect('404');}
+    const product = db.products.find(p => p.id === productId);
+    if (!product){ return res.redirect('404'); }
+    res.render('productDetails', {product});
+}
+
 exports.getCreateProductPage = (req, res) => {
     res.render('createProduct');
 };
--- secondDemo/routes.js
+++ secondDemo/routes.js
@@ -10,6 +10,8 @@ router.get('/add-product', productController.getCreateProductPage);
 
 router.post('/save-product', productController.saveProduct);
 
+router.get('/product/:id', productController.getProductDetailsPage);
+
 router.get('**', staticPagesController.get404Page);
 
 module.exports = router;
