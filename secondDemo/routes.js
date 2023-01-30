ede6156decb321643a430936631bdcff9164ac80
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-30 14:01:36 +0200

    create accessory middleware

--- secondDemo/routes.js
+++ secondDemo/routes.js
@@ -1,4 +1,5 @@
 const productController = require("./controllers/CubeController");
+const accessoryController = require('./controllers/AccessoryController');
 const staticPagesController = require("./controllers/StaticPageController");
 const router = require('express').Router();
 
@@ -11,6 +12,9 @@ router.get('/add-product', productController.getCreateProductPage);
 router.post('/save-product', productController.saveProduct);
 
 router.get('/product/:id', productController.getProductDetailsPage);
+
+//If path starts with /accessory send it to accessoryController
+router.use('/accessory', accessoryController);
 
 router.get('**', staticPagesController.get404Page);
 
