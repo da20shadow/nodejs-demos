2d0201103c108b6b56717848db73f4cb4068b2ec
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-30 14:20:51 +0200

    Created AccessoryController

---
+++ secondDemo/controllers/AccessoryController.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const Accessory = require('../models/Accessory')
+
+//URL: /accessory/create
+router.get('/create', (req, res) => {
+    res.render('accessory/create');
+});
+
+router.post('/create', async (req, res) => {
+    const {name,description,category,price,imgUrl} = req.body;
+
+    const createdAccessory = await Accessory.create({name, description, category, price, imgUrl});
+
+    console.log(createdAccessory);
+
+    res.redirect('/');
+});
+
+module.exports = router;
