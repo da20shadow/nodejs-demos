09cf4d0bd2996987eecc072d41c4dc03b5028f21
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-09 18:10:05 +0200

    Create db ini

---
+++ ToDoApp/config/dbini.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const config = require('./environments');
+
+async function dbInit(){
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.DB_URI); //If there is no connection will not print the log!
+    console.log('DB Connected!');
+}
+
+module.exports = dbInit;
