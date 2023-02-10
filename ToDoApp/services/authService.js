398971ab3d8997af6b5aa586865da32d09ed0eae
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-10 11:05:05 +0200

    login implemented

--- ToDoApp/services/authService.js
+++ ToDoApp/services/authService.js
@@ -1,5 +1,15 @@
 const User = require('../models/User');
 
+exports.login = async (username, password) => {
+    //in node js "this" in module is here in the module not in the global context
+    const user = await this.getUserByUsername(username);
+
+    const isValid = await user.verifyPassword(password);
+    if (!user || !isValid){
+        throw 'Invalid username or password!';
+    }
+}
+
 exports.getUserByUsername = async (username) => {
     return User.findOne({username}); //When return no need to use await
 }
