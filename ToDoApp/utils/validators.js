c83cb7d2ab2b30ce2eed42cfa82009a8c99c23b7
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-10 17:11:13 +0200

    fixed patern

--- ToDoApp/utils/validators.js
+++ ToDoApp/utils/validators.js
@@ -1,5 +1,5 @@
 function validateEmail(value) {
-    return /^[a-z]+[a-z0-9_]+[@][a-z][.][a-z]{2,11}$/.test(value);
+    return /^[a-z]+[a-z0-9_]+[@][a-z]+[.][a-z]{2,11}$/.test(value);
 }
 
 module.exports = {
