68d783490ccf9eb5b093255a43d575745dcb4af0
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-10 17:11:05 +0200

    added email

--- ToDoApp/services/authService.js
+++ ToDoApp/services/authService.js
@@ -18,6 +18,6 @@ exports.getUserByUsername = async (username) => {
     return User.findOne({username}); //When return no need to use await
 }
 
-exports.register = async (username,plainPassword) => {
-    return User.create({username,password:plainPassword});
+exports.register = async (username,email,plainPassword) => {
+    return User.create({username,email,password:plainPassword});
 };
