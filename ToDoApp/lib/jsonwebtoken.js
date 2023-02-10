820d2de07d00733d8204d0abc33413f8e5338357
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-10 17:08:18 +0200

    convert calback to promise

--- ToDoApp/lib/jsonwebtoken.js
+++ ToDoApp/lib/jsonwebtoken.js
@@ -1,10 +1,22 @@
 const util = require('util');
 const jwtCallBack = require('jsonwebtoken');
 
-//This function get callback function and transform it to promise
+//This function promisify gets callback function and transform it to promise
 const jwt = {
     sign: util.promisify(jwtCallBack.sign),
     verify: util.promisify(jwtCallBack.verify),
 };
+
+//Here is what promisify do inside
+function promiseSign(payload, secret, options) {
+    return new Promise(function (resolve, reject) {
+        jwtCallBack.sign(payload, secret, options, (err, token) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(token);
+        });
+    });
+}
 
 module.exports = jwt;
