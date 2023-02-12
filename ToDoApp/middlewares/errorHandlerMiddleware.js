4477557aae7cfa7497e89ee44851d772a7dc6a06
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-12 15:40:01 +0200

    Genering error handler

--- ToDoApp/middlewares/errorHandlerMiddleware.js
+++ ToDoApp/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,8 @@
 const errorHandler = (err,req,res,next) => {
     console.log(err.message);
+    //Here can redirect to 404 for example and show error message
+    //For example someone check product then in url add /edit and will access edit page
+    //But here we can redirect him to 404 and say you can't edit product (stupid but yes)
     next();
 }
 module.exports = {
