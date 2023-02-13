ae6c1ea8f73cb3e61fae6f45297527e74bb77ec1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-13 15:20:59 +0200

    export all constants

---
+++ cryptoTrading/constants/index.js
@@ -0,0 +1,11 @@
+const jwtConstants = require('./jwtSecret');
+const regexPatterns = require('./regexPatterns');
+const schemaRules = require('./schemaRules');
+const errorMessages = require('./errorMessages');
+
+module.exports = {
+    jwtConstants,
+    regexPatterns,
+    schemaRules,
+    errorMessages,
+}
