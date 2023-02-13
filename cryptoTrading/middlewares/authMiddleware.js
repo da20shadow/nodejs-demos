1ca0f8dc2da2f5657fd8ab4935b9e07ffaf89523
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-13 14:38:57 +0200

    created auth middleware

---
+++ cryptoTrading/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("../lib/jsonwebtoken");
+const {JWT_SECRET} = require('../constants');
+
+exports.authentication = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        //private user
+        try {
+            const decodedToken = await jwt.verify(token, JWT_SECRET);
+            req.user = decodedToken;
+            req.isAuthenticated = true;
+            res.locals.username = decodedToken.username;
+            res.locals.isAuthenticated = true; //Will get this variable in navigation
+        } catch (err) {
+            console.log(err);
+            res.clearCookie('auth');
+            return res.redirect('/404');
+        }
+    }
+    next();
+}
+
+exports.isAuthenticated = (req, res, next) => {
+    if (!req.isAuthenticated) {
+        return res.redirect('/404');
+    }
+    next();
+}
