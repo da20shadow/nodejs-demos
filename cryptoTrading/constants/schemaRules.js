c5ac5bec5881551e5f2e9f33ec59292b68cd125b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-13 15:18:57 +0200

    create constants for schema rules

---
+++ cryptoTrading/constants/schemaRules.js
@@ -0,0 +1,12 @@
+//Min max validation rules
+const minUsernameLength = 3;
+const maxUsernameLength = 45;
+const minPasswordLength = 8;
+const maxPasswordLength = 45;
+
+module.exports = {
+    minUsernameLength,
+    maxUsernameLength,
+    minPasswordLength,
+    maxPasswordLength,
+}
