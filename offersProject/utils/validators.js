2388d49d67380cf5658f6815d7b5d35fdf8aee46
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-14 15:26:04 +0200

    Create util validators

---
+++ offersProject/utils/validators.js
@@ -0,0 +1,23 @@
+const {schemaRules, regexPatterns} = require("../constants");
+
+function isValidEmail(emailInput) {
+    return regexPatterns.emailPattern.test(emailInput);
+}
+
+function isValidUsername(usernameInput) {
+    if (usernameInput.length < schemaRules.minUsernameLength || usernameInput.length > schemaRules.maxUsernameLength) {
+        return false;
+    }
+    return regexPatterns.usernamePattern.test(usernameInput);
+}
+
+function isValidPassword(passwordInput) {
+    if (passwordInput.length < schemaRules.minPasswordLength || passwordInput > schemaRules.maxPasswordLength) {
+        return false;
+    }
+}
+
+module.exports = {
+    isValidEmail,
+    isValidUsername
+};
