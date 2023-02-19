9835098ed369353cfbe51989be50ead23d802061
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:34:36 +0200

    Create error messages

---
+++ petstagram/constants/errorMessages.js
@@ -0,0 +1,38 @@
+const invalidEmail = "Please, enter valid email!";
+const invalidFields = "All fields are required!";
+const invalidLoginDetails = "Wrong username or password!";
+const passMismatch = "Password mismatch!";
+const existingUser = "Such user already registered!";
+const notFoundUser = "User not found!";
+// const getThrowErrorMessage = (err) => {
+//     let error = err.message;
+//     if (err.errors) {
+//         if (err.errors.username) {
+//             error = err.errors.username.message;
+//         } else if (err.errors.email) {
+//             error = err.errors.email.message;
+//         } else if (err.errors.password) {
+//             error = err.errors.password.message;
+//         }
+//     }
+//     return error;
+// }
+const getThrowErrorMessage = (err) => {
+    if (err.errors){
+        return Object.values(err.errors)[0].message;
+    }
+    return err.message;
+}
+const petNotExist = "Post Not Found!";
+
+module.exports = {
+    invalidEmail,
+    invalidFields,
+    invalidLoginDetails,
+    passMismatch,
+    existingUser,
+    getThrowErrorMessage,
+    petNotExist,
+    notFoundUser
+};
+
