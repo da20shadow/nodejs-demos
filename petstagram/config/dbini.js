0c87560a7b253b4a27880e53ae37336b1fad2a95
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:33:41 +0200

    Create db ini

---
+++ petstagram/config/dbini.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const config = require('./environments');
+
+async function dbInit(){
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.DB_URI);
+    console.log('DB Connected!');
+}
+
+module.exports = dbInit;
