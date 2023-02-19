fa19b866adfd5674e4469b4a75168eb56a398d85
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:37:57 +0200

    create validators

---
+++ petstagram/utils/validators.js
@@ -0,0 +1,28 @@
+const {schemaRules, regexPatterns} = require("../constants");
+
+function isValidEmail(emailInput) {
+    return regexPatterns.emailPattern.test(emailInput);
+}
+
+function isValidURL(url) {
+    return regexPatterns.urlPattern.test(url);
+}
+
+function isValidUsername(usernameInput) {
+    if (usernameInput.length < schemaRules.minUsernameLength || usernameInput.length > schemaRules.maxUsernameLength) {
+        return false;
+    }
+    return regexPatterns.usernamePattern.test(usernameInput);
+}
+
+function isValidPassword(passwordInput) {
+    if (passwordInput.length < schemaRules.minPasswordLength || passwordInput > schemaRules.maxPasswordLength) {
+        return false;
+    }
+}
+
+module.exports = {
+    isValidEmail,
+    isValidUsername,
+    isValidURL
+};
