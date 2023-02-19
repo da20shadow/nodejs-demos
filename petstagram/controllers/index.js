a3f9045d0b2ded389d074e86142130fc14031dff
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:39:50 +0200

    export controllers with index file

---
+++ petstagram/controllers/index.js
@@ -0,0 +1,11 @@
+const authController = require('./authController');
+const staticPagesController = require('./staticPagesController');
+const petsController = require('./petsController');
+const userController = require('./userController');
+
+module.exports = {
+    authController,
+    staticPagesController,
+    petsController,
+    userController
+}
