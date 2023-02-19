fa7dd000315a7d7402d8d7c01a02c4748e8d221c
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:35:26 +0200

    create index for exporting all constants

---
+++ petstagram/constants/index.js
@@ -0,0 +1,11 @@
+const JWTConstants = require('./jwt');
+const regexPatterns = require('./regexPatterns');
+const schemaRules = require('./schemaRules');
+const errorMessages = require('./errorMessages');
+
+module.exports = {
+    JWTConstants,
+    regexPatterns,
+    schemaRules,
+    errorMessages,
+}
