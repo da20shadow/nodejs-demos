c65bed41c4d297fd307df466c72085113e98443e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-19 18:35:36 +0200

    create schema rules consts

---
+++ petstagram/constants/schemaRules.js
@@ -0,0 +1,11 @@
+//Min max validation rules
+const minUsernameLength = 2;
+const maxUsernameLength = 45;
+const minPasswordLength = 4;
+
+
+module.exports = {
+    minUsernameLength,
+    maxUsernameLength,
+    minPasswordLength,
+}
